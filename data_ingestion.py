cd62d4acd548c00598ee08677df3097708820288
@mamakjhb025 <mamakjhb025@student.wethinkcode.co.za>
2026-09-10 13:54:48 +0200

    Downloading ticker data from Yahoo finance, Data engineering project is a go

---
+++ data_ingestion.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+# Declare the date to download the ticker symbol data from the last 12 months
+current_time = datetime.now()
+start_time = (current_time - relativedelta(months=12)).strftime("%Y-%m-%d")
+end_time = current_time.strftime("%Y-%m-%d")
+
+# Declare the ticker symbol to download the data for
+ticker_symbols = ["AAPL", "MSFT", "GOOGL", "SPY"]
+# Download the data for the ticker symbol from Yahoo Finance
+
+data = yf.download(ticker_symbols, start=start_time, end=end_time, auto_adjust=True)
+print(data)
