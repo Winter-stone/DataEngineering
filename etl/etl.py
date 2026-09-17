f063f5eaeed14ae81b33ed3e01ab0fd361451de4
Winter-stone <marvinmontic7@gmail.com>
2026-09-17 08:25:15 +0200

    Update: Updated directory references from the imports

--- etl/etl.py
+++ etl/etl.py
@@ -1,15 +1,15 @@
 import pandas as pd
-import persistance.save_data as sd
-import etl.data_transformation as dt
+import clean_data as cd
+import data_transformation as dt
 
 data = pd.read_parquet("stock_data.parquet", engine="pyarrow")
-data = sd.clean_data(data)
+data = cd.deep_clean(data)
 
 filters = dt.filters(data)
 
 data = filters.get_all_filters()
 
-data = sd.clean_data(data)
+data = cd.deep_clean(data)
 
 
 print(data.head())
